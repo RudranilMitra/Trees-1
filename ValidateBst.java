ca3287bbdc23cb7bbcf8a57764edf2385f435b3e
RudranilMitra <62679743+RudranilMitra@users.noreply.github.com>
2023-08-23 21:32:36 -0700

    Create ValidateBst.java

---
+++ ValidateBst.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode prev;
+    boolean flag = true;
+    public boolean isValidBST(TreeNode root) {
+        inOrderTraversal(root);
+        return flag;
+    }
+
+    public void inOrderTraversal(TreeNode root){
+        if(!flag)
+            return;
+        if(root==null)
+            return;
+        inOrderTraversal(root.left);
+        if(prev != null && prev.val >= root.val){
+            flag = false;
+        }
+        prev = root;
+        inOrderTraversal(root.right);
+    }
+}
